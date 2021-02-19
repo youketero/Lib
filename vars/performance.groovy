e52412cde604b8e460975b5abe321c278c3fb2ec
Andrii Tarnovetskyi (CLGX) <atarnovetskyi@corelogic.com>
2021-02-19 18:17:29 +0200

    add some functions

--- vars/performance.groovy
+++ vars/performance.groovy
@@ -1,3 +1,13 @@
 def hello(){
 	return "Hello dear user"
 }
+
+
+def choiceRememberLastSelected(List<String> paramsChoice, String nameChoice, String descriptionChoice) {
+    String options = createChoicesWithPreviousChoice(paramsChoice,params[nameChoice]).join("\n")
+    return choice(
+        name: nameChoice, 
+        choices: options,
+        description: descriptionChoice
+    )
+}
